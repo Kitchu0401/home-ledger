f250d66b85e2baf1644f4bdaf44c77a223c97147
Kitchu0401 <kitchu@naver.com>
2017-04-21 23:55:38 +0900

    component interaction mid-commit

---
+++ child.vue
@@ -0,0 +1,14 @@
+<template>
+    <h3>{{ message }}</h3>
+</template>
+
+<script>
+    export default {
+        props: ['message'],
+        methods: {
+            sendMessage() {
+                this.$emit('alert')
+            }
+        }
+    } 
+</script>
---
+++ parent.vue
@@ -0,0 +1,15 @@
+<template>
+    <children-component 
+        message="data"
+        v-on:alert="alertMessage"/>
+</template>
+
+<script>
+    export default {
+        methods: {
+            alertMessage() {
+                alert('Hi!');
+            }
+        }
+    }
+</script>
