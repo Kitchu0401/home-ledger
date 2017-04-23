e20c4170e5aeda9a7937f51e70fd2f4d150e835e
Kitchu0401 <kitchu@naver.com>
2017-04-23 23:46:30 +0900

    test commit

---
+++ frontend/src/components/Chart.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="chart"></div>
+</template>
+
+<script>
+export default {
+    created() {
+        
+    },
+    data() {
+        return {
+            
+        };
+    }
+};
+</script>
+
+<style>
+
+</style>
