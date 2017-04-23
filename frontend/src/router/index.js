4b93b5892e1e21eb74fa0b5df269b3f157dba891
Kitchu0401 <kitchu@naver.com>
2017-04-23 23:47:24 +0900

    initial transform commit

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,22 +1,20 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import IndexPage from '../components/IndexPage';
-import ShowPage from '../components/ShowPage';
 
 Vue.use(Router);
 
 export default new Router({
   mode: 'history',
   routes: [
-    {
-      path: '/',
-      name: 'indexPage',
-      component: IndexPage
-    },
-    {
-      path: '/:id',
-      name: 'showPage',
-      component: ShowPage
-    }
+    // {
+    //   path: '/',
+    //   name: 'indexPage',
+    //   component: IndexPage
+    // },
+    // {
+    //   path: '/:id',
+    //   name: 'showPage',
+    //   component: ShowPage
+    // }
   ]
 });
