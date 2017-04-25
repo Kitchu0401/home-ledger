3c07f4966cebaaa90337343de3bf486ca662a643
Kitchu0401 <kitchu@naver.com>
2017-04-26 00:04:28 +0900

    added modifying process for receipt list api. (later move to mongodb aggregate function.)

--- backend/routes/receipt.js
+++ backend/routes/receipt.js
@@ -5,7 +5,22 @@ var router = express.Router();
 
 // GET receipt list
 router.get('/', function(req, res, next) {
-    res.send(receipts);
+    var modifiedResult = receipts.reduce((prev, curr) => {
+        if ( !prev[curr.date] ) {
+            prev[curr.date] = {
+                date: curr.date,
+                totalAmount: curr.type === '0' ? curr.amount : curr.amount * -1,
+                receipts: [curr]
+            }
+        } else {
+            prev[curr.date].totalAmount = curr.type === '0' ? prev[curr.date].totalAmount + curr.amount : prev[curr.date].totalAmount - curr.amount;
+            prev[curr.date].receipts.push(curr);
+        }
+
+        return prev;
+    }, {});
+
+    res.send(modifiedResult);
 });
 
 // GET movie
