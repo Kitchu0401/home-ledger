39a21129e5ab47113e83629f8374105a8245a714
Kitchu0401 <kitchu@naver.com>
2017-05-08 20:20:17 +0900

    fixed publicPath for production build

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -9,7 +9,8 @@ module.exports = {
     // index: path.resolve(__dirname, '../dist/index.html'),
     // assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/HomeLedger',
+    // MODIFED: appended for application name
+    assetsPublicPath: '/HomeLedger/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
@@ -30,6 +31,7 @@ module.exports = {
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {
+      // MODIFED: dev server has no application name
       '/HomeLedger/api': {
         target: 'http://localhost:3000',
         changeOrigin: true,
